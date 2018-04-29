45734e1ed8837918aba466a31b30792455ac4aab
Kourtney Reynolds <Kourtney.Reynolds@gmail.com>
2018-04-29 13:46:41 -0700

    Connected the htmlroutes to the server and am able to navigate based on the GET request being sent the the server for specific html pages

---
+++ server.js
@@ -0,0 +1,34 @@
+// ==============================================================================
+// DEPENDENCIES
+
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+// ==============================================================================
+// EXPRESS CONFIGURATION
+
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+// ================================================================================
+// ROUTER
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
+
+// require("./routes/apiRoutes")(app);//API Routes 
+require("./app/routes/htmlRoutes")(app);//Html Routes 
+
+// =============================================================================
+// LISTENER
+
+
+
+app.listen(PORT, ()=> {
+  console.log("App listening on PORT: " + PORT);
+});
