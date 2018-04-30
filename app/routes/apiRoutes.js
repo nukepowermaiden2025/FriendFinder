f6928eead74e48cd26956c320e655d25f9c48228
Kourtney Reynolds <Kourtney.Reynolds@gmail.com>
2018-04-29 21:12:39 -0700

    Tageting the selector correctly and am able to pass data to scores array properly

--- app/routes/apiRoutes.js
+++ app/routes/apiRoutes.js
@@ -30,7 +30,7 @@ module.exports = function(app) {
   // ---------------------------------------------------------------------------
 
 //   THIS IS WHERE THE FRIEND COMPATIBILITY WILL BE HANDLED
-  app.post("/api/survey", function(req, res) {
+  app.post("/api/friendsData", function(req, res) {
     // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
     // It will do this by sending out the value "true" have a table
     // req.body is available since we're using the body-parser middleware
