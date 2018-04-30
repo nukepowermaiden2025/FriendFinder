44f7fd978396010e1448ce556cb5f0d18f12e036
Kourtney Reynolds <Kourtney.Reynolds@gmail.com>
2018-04-29 18:59:47 -0700

    Add the logic to generate divs from questions array and html is being added properly

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(bodyParser.json());
 // ROUTER
 // These routes give our server a "map" of how to respond when users visit or request data from various URLs.
 
-// require("./routes/apiRoutes")(app);//API Routes 
+require("./app/routes/apiRoutes")(app);//API Routes 
 require("./app/routes/htmlRoutes")(app);//Html Routes 
 
 // =============================================================================
